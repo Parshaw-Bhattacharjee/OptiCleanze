423183b5bacbdfbf16f99f1ba1b2e6f88cc1d3c7
Parshaw-Bhattacharjee <parshawbhattacharjee@gmail.com>
2024-03-06 23:44:23 +0530

    added remove_temp function in the main

--- libraries/remove_temps.py
+++ libraries/remove_temps.py
@@ -17,37 +17,38 @@ temp_ext = [
     '.cache'
 ]
 
-for dir in temp_dirs:
-    for dir_path, dir_name, file_names in os.walk(os.path.expanduser(dir)):
-        for file_name in file_names:
-            # check to see if the file extension is in the list of temporary files extension
-            if os.path.splitext(file_name)[1].lower() in temp_ext:
-                file_path = os.path.join(dir_path, file_name)
-                try:
-                    if os.path.isfile(file_path):
-                        os.remove(file_path)
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
-
-                    f = open("../logs/remove_temps.log", "w")
-                    f.write(f"{datetime.now()}: Removed {file_path}\n")
-                    f.close()
-                except Exception as e:
-                    f = open("../logs/remove_temps_error.log", "w")
-                    f.write(f"{datetime.now()}: Error deleting {file_path}: {e}\n")
-
-                # Remove screenshots from the Desktop
-                if dir == '~/Desktop':
-                    desktop_path = os.path.expanduser(dir) # expand ~ to /home directory
-                    screenshot_files = [f for f in os.listdir(desktop_path) if f.startswith('Screenshot')]
-                    for file in screenshot_files:
-                        file_path = os.path.join(desktop_path, file)
-                        try:
+def remove_temps():
+    for dir in temp_dirs:
+        for dir_path, dir_name, file_names in os.walk(os.path.expanduser(dir)):
+            for file_name in file_names:
+                # check to see if the file extension is in the list of temporary files extension
+                if os.path.splitext(file_name)[1].lower() in temp_ext:
+                    file_path = os.path.join(dir_path, file_name)
+                    try:
+                        if os.path.isfile(file_path):
                             os.remove(file_path)
-                            f = open("../logs/remove_temps.log", "w+")
-                            f.write(f"{datetime.now()}: Removed {file_path}\n")
-                            f.close()
-                        except Exception as e:
-                            f = open("../logs/remove_temps_error.log", "w")
-                            f.write(f"{datetime.now()}: Error deleting {file_path}: {e}\n")
-                            f.close()
+                        elif os.path.isdir(file_path):
+                            shutil.rmtree(file_path)
+
+                        f = open("../logs/remove_temps.log", "w")
+                        f.write(f"{datetime.now()}: Removed {file_path}\n")
+                        f.close()
+                    except Exception as e:
+                        f = open("../logs/remove_temps_error.log", "w")
+                        f.write(f"{datetime.now()}: Error deleting {file_path}: {e}\n")
+
+                    # Remove screenshots from the Desktop
+                    if dir == '~/Desktop':
+                        desktop_path = os.path.expanduser(dir) # expand ~ to /home directory
+                        screenshot_files = [f for f in os.listdir(desktop_path) if f.startswith('Screenshot')]
+                        for file in screenshot_files:
+                            file_path = os.path.join(desktop_path, file)
+                            try:
+                                os.remove(file_path)
+                                f = open("../logs/remove_temps.log", "w+")
+                                f.write(f"{datetime.now()}: Removed {file_path}\n")
+                                f.close()
+                            except Exception as e:
+                                f = open("../logs/remove_temps_error.log", "w")
+                                f.write(f"{datetime.now()}: Error deleting {file_path}: {e}\n")
+                                f.close()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from libraries import remove_duplicates
+from libraries import remove_duplicates, remove_temps
 
 dirs = "/home/parshaw/Downloads"
 
 remove_duplicates.remove_duplicates(dirs)
+
+remove_temps.remove_temps()
