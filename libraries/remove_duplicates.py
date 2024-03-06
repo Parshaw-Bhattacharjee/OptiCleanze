92fe0439e97459425a4a5e7660ad2bf93f3d95f1
Parshaw-Bhattacharjee <parshawbhattacharjee@gmail.com>
2024-03-06 23:42:05 +0530

    minor fixes added temporary files removal

--- libraries/remove_duplicates.py
+++ libraries/remove_duplicates.py
@@ -1,4 +1,5 @@
-import os, re, datetime
+import os, re
+from datetime import datetime
 
 def remove_duplicates(directory):
     f = open("logs/remove_duplicates.log", "w")
@@ -24,15 +25,15 @@ def remove_duplicates(directory):
         for filename in files_dict:
             file_detail = files_dict[filename]
             file_detail.sort(key = lambda file_name: file_name[0])
-            f.write(f"{datetime.datetime.today()}: File detail {file_detail}")
+            f.write(f"{datetime.today()}: File detail {file_detail}")
             latest_file_path = file_detail[-1][1]
             latest_file_extension = file_detail[-1][2]
 
             for file_number, file_path, _ in file_detail[:-1]:
-                f.write(f"{datetime.datetime.today()}: Deleted duplicate file: {file_path}\n")
+                f.write(f"{datetime.today()}: Deleted duplicate file: {file_path}\n")
                 os.remove(file_path)
 
             os.rename(latest_file_path, os.path.join(dir, f"{file_name}{latest_file_extension}"))
-            f.write(f"{datetime.datetime.today()}: Rename {latest_file_path} to {file_name}{latest_file_extension}\n")
+            f.write(f"{datetime.today()}: Rename {latest_file_path} to {file_name}{latest_file_extension}\n")
 
     f.close()
---
+++ libraries/remove_temps.py
@@ -0,0 +1,53 @@
+import os, shutil
+from datetime import datetime
+
+# Directories to search for temporary files
+
+temp_dirs = [
+    '~/Downloads',
+    '~/Desktop'
+    'C:/Users/Appdata/Local/Temp'
+]
+
+# Extensions of the temporary files
+
+temp_ext = [
+    '.log',
+    '.tmp',
+    '.cache'
+]
+
+for dir in temp_dirs:
+    for dir_path, dir_name, file_names in os.walk(os.path.expanduser(dir)):
+        for file_name in file_names:
+            # check to see if the file extension is in the list of temporary files extension
+            if os.path.splitext(file_name)[1].lower() in temp_ext:
+                file_path = os.path.join(dir_path, file_name)
+                try:
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+
+                    f = open("../logs/remove_temps.log", "w")
+                    f.write(f"{datetime.now()}: Removed {file_path}\n")
+                    f.close()
+                except Exception as e:
+                    f = open("../logs/remove_temps_error.log", "w")
+                    f.write(f"{datetime.now()}: Error deleting {file_path}: {e}\n")
+
+                # Remove screenshots from the Desktop
+                if dir == '~/Desktop':
+                    desktop_path = os.path.expanduser(dir) # expand ~ to /home directory
+                    screenshot_files = [f for f in os.listdir(desktop_path) if f.startswith('Screenshot')]
+                    for file in screenshot_files:
+                        file_path = os.path.join(desktop_path, file)
+                        try:
+                            os.remove(file_path)
+                            f = open("../logs/remove_temps.log", "w+")
+                            f.write(f"{datetime.now()}: Removed {file_path}\n")
+                            f.close()
+                        except Exception as e:
+                            f = open("../logs/remove_temps_error.log", "w")
+                            f.write(f"{datetime.now()}: Error deleting {file_path}: {e}\n")
+                            f.close()
